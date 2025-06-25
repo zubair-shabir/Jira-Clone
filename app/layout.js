4d2372ab85776fac6eac32ed44b76330c7923044
zubair-shabir <zubairshabir980@gmail.com>
2025-06-26 01:02:17 +0530

    Landing Page Completed

--- app/layout.js
+++ app/layout.js
@@ -16,39 +16,40 @@ const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children }) {
   return (
-      <ClerkProvider appearance={{baseTheme:shadesOfPurple,
-         variables: {
-          colorPrimary: "#3b82f6",
-          colorBackground: "#1a202c",
-          colorInputBackground: "#2D3748",
-          colorInputText: "#F3F4F6",
-        },
-        elements: {
-          formButtonPrimary: "bg-purple-600 hover:bg-purple-700 text-white",
-          card: "bg-gray-800",
-          headerTitle: "text-blue-400",
-          headerSubtitle: "text-gray-400",
-        },
-      }}
-      >
-    <html lang="en">
-      <body
-        className={`${inter.className} `}
-      >
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Header />
-          <main className="min-h-screen">
+    <ClerkProvider appearance={{
+      baseTheme: shadesOfPurple,
+      variables: {
+        colorPrimary: "#3b82f6",
+        colorBackground: "#1a202c",
+        colorInputBackground: "#2D3748",
+        colorInputText: "#F3F4F6",
+      },
+      elements: {
+        formButtonPrimary: "bg-purple-600 hover:bg-purple-700 text-white",
+        card: "bg-gray-800",
+        headerTitle: "text-blue-400",
+        headerSubtitle: "text-gray-400",
+      },
+    }}
+    >
+      <html lang="en">
+        <body
+          className={`${inter.className} dotted-background `}
+        >
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
+            <main className="min-h-screen">
 
-            {children}
-          </main>
-          <footer className="bg-gray-900 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p > Made With ❤️ By Zubair Shabir</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+              {children}
+            </main>
+            <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p > Made With ❤️ By Zubair Shabir</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
---
+++ app/page.jsx
@@ -0,0 +1,137 @@
+import CompanyCarousel from "@/components/companyCarousal";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { ArrowRight, BarChart, Calendar, ChevronRight, Layout } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import faqs from "@/data/faqs";
+
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workflow and optimize team productivity with our easy-to-use Kanban boards.",
+    icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    icon: BarChart,
+  },
+];
+
+
+export default function Home() {
+
+
+  return (
+    <div className="min-h-screen">
+      {/* Hero */}
+      <section className="container mx-auto py-20 text-center">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">Streamline your Workflow <br/>
+         <span className="flex mx-auto gap-3 sm:gap-4 items-center pt-4">
+            with
+            <Image
+              src={"/logo2.png"}
+              alt="Zscrum Logo"
+              width={400}
+              height={80}
+              className="h-14 sm:h-24 w-auto object-contain"
+            />
+          </span>
+        </h1>
+        <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
+          Empower your team with our intuitive project management solution.
+        </p>
+        <p className="text-xl mb-12 max-w-2xl mx-auto"></p>
+        <Link href="/onboarding">
+          <Button size="lg" className="mr-4">
+            Get Started <ChevronRight size={18} className="ml-1" />
+          </Button>
+        </Link>
+        <Link href="#features">
+          <Button size="lg" variant="outline">
+            Learn More
+          </Button>
+        </Link>
+      </section>
+
+{/* Features */}
+           <section id="features" className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Key Features</h3>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => (
+              <Card key={index} className="bg-gray-800">
+                <CardContent className="pt-6">
+                  <feature.icon className="h-12 w-12 mb-4 text-blue-300" />
+                  <h4 className="text-xl font-semibold mb-2">
+                    {feature.title}
+                  </h4>
+                  <p className="text-gray-300">{feature.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
+          {/* Companies Carousel */}
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">
+            Trusted by Industry Leaders
+          </h3>
+          
+
+          <CompanyCarousel />
+          
+        </div>
+      </section>
+
+
+         {/* FAQ Section */}
+      <section className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">
+            Frequently Asked Questions
+          </h3>
+          <Accordion type="single" collapsible className="w-full">
+            {faqs.map((faq, index) => (
+              <AccordionItem key={index} value={`item-${index}`}>
+                <AccordionTrigger>{faq.question}</AccordionTrigger>
+                <AccordionContent>{faq.answer}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
+      </section>
+
+            {/* CTA Section */}
+      <section className="py-20 text-center px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-6">
+            Ready to Transform Your Workflow?
+          </h3>
+          <p className="text-xl mb-12">
+            Join thousands of teams already using ZCRUM to streamline their
+            projects and boost productivity.
+          </p>
+          <Link href="/onboarding">
+            <Button size="lg" className="animate-bounce">
+              Start For Free <ArrowRight className="ml-2 h-5 w-5" />
+            </Button>
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+}
